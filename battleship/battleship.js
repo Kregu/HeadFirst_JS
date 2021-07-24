1e314e8cfd207994f2a808c852c1f2544ec07615
Sergey Gorokhov <sergey.login@gmail.com>
2021-07-24 23:30:14 +0300

    improve battleship program

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -1,15 +1,24 @@
-console.log("babah");
 var position;
 var location1, location2, location3;
 var guess;
 var guesses = hits = 0;
 var isSunk = false;
+var randomLock
 
-location1 = Math.floor(Math.random() * (5));
+randomLock =Math.floor(Math.random() * (5));
+
+location1 = randomLock;
 location2 = location1 + 1;
 location3 = location2 + 1;
 
+var map = [false, false, false, false, false, false, false]
+
+map[location1] = true
+map[location2] = true
+map[location3] = true
+
 console.log(location1, location2, location3);
+console.log(map);
 
 while (isSunk==false){
 	guess = prompt("Enter your target (0..6):");
@@ -20,14 +29,17 @@ while (isSunk==false){
 		guesses += 1;
 	}
 
-	if (guess == location1 || guess == location2 || guess ==location3){
+	if (map[guess]){
 
+		map[guess] = false
 		hits += 1;
+		alert("HIT!");
+
 		if (hits == 3){
 			isSunk = true;
 			alert("You sank my battleship!")
 		}
-		alert("HIT!");
+
 	}
 	else{
 		alert("MISS");
