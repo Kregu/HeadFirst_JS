8498cad88b1fc2e184aa50e90a309e1deed35c7c
Sergey Gorokhov <sergey.login@gmail.com>
2021-08-28 00:14:18 +0300

    add demo prototype

---
+++ prototype_demo.js
@@ -0,0 +1,44 @@
+function Dog(name, breed, weight){
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+}
+
+Dog.prototype.species = "Canin";
+
+Dog.prototype.bark = function (){
+    if (this.weight > 25){
+        console.log(this.name + " says Woof!");
+    } else {
+        console.log(this.name + " says Yip!");
+    }
+};
+Dog.prototype.run = function (){
+    console.log("Run!");
+};
+Dog.prototype.wag = function (){
+    console.log("Wag!");
+};
+
+const fido = new Dog("Fido", "Mixed", 38);
+const fluffy = new Dog("Fluffy","Mixed", 30);
+const spot = new Dog("Spot", "Chihuahua", 10);
+
+spot.bark = function (){
+    console.log(this.name + " says Wof!")
+}
+
+fido.bark()
+fido.run()
+fido.wag()
+
+fluffy.bark()
+fluffy.run()
+fluffy.wag()
+
+spot.bark()
+spot.run()
+spot.wag()
+
+
+
